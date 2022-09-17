26b3712e487382a87ff295f1e84e5ff281701cf6
anny <puchkovdd@gmail.com>
2022-09-17 15:01:04 +0300

    created api for users

---
+++ src/users/users.model.ts
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+import { Model, Table, Column } from "sequelize-typescript";
+
+interface UserCreationAttrs {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: "users" })
+export class User extends Model<User, UserCreationAttrs> {
+  @Column({
+    type: DataTypes.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+  @Column({ type: DataTypes.STRING, unique: true, allowNull: false })
+  email: string;
+  @Column({ type: DataTypes.STRING, allowNull: false })
+  password: string;
+  @Column({ type: DataTypes.BOOLEAN, defaultValue: false })
+  banned: boolean;
+  @Column({ type: DataTypes.STRING, allowNull: true })
+  banReason: string;
+}
